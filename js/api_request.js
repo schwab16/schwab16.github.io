c9bad01b0dfbd015f49e24109792b2462c798686
Isaac <schw1643@umn.edu>
2017-02-01 13:34:47 -0600

    added key

--- js/api_request.js
+++ js/api_request.js
@@ -1,5 +1,6 @@
 app.get('/test', function(req, res, next) {
   var context = {};
+  var apiKey = '213130C1164F29F16CBBA10074759DB0'
   var url = 'https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=' + apiKey;
   request(url, function(err, response, body) {
     if (!err && response.statusCode < 400) {
